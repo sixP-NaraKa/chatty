ccd6757e3f1064cd2ae27cc5bc66df800603fdb0
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2022-06-16 19:29:00 +0200

    DEV: init tailwindcss. Introduce new 'npm run serve' command to actually run application on port 4300.

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: 9976,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
