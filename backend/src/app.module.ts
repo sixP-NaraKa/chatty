df596aea4dc4f0fb6d778e492e2f6e7a6edb8362
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2022-06-17 18:32:38 +0200

    DEV - WIP: setup NestJS (backend), adjust workspace structure into 'frontend' (Angular) and 'backend' (NestJS).

---
+++ backend/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname, "../../frontend", "dist/chatty")
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- frontend/src/app/tabs/tabs.component.ts
+++ frontend/src/app/tabs/tabs.component.ts
@@ -1,5 +1,14 @@
 import { AfterContentInit, Component, ContentChildren, EventEmitter, Input, Output, QueryList } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
+import { chatty, PrismaClient, PrismaPromise } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+type Chat = {
+  chatId: number,
+  chatPartnerId: number,
+  chatPartnerName: string
+}
 
 @Component({
   selector: 'app-tabs',
@@ -14,7 +23,25 @@ export class TabsComponent implements AfterContentInit {
   @Output()
   loadChat = new EventEmitter();
 
-  ngAfterContentInit() {
+  // availableChats!: Promise<{ chat_id: number; }[]>;
+  // availableChats!: PrismaPromise<chatty[]>;
+  availableChats!: PrismaPromise<chatty[]>;
+
+  constructor() {
+      // load chats (tabs to click on) from the db
+      // this.availableChats = prisma.chatty.findMany({
+      //   // select: {
+      //   //   chat_id: true
+      //   // },
+      //   where: {
+      //     chat_id: 1
+      //   }
+      // });
+      // console.dir(this.availableChats, { depth: null });
+  }
+
+
+  async ngAfterContentInit() {
     let activeTabs = this.tabs.filter((tab) => tab.active);
     if (activeTabs.length === 0) {
       this.selectTab(this.tabs.first);
