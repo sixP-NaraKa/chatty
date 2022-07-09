b5ca5085e1389e80ea810f890f62f798fe676e7e
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2022-07-09 23:10:46 +0200

    (#25): add filtering to emote selection (user can filter them via text input).

--- frontend/src/app/emote-select/emote-select.component.ts
+++ frontend/src/app/emote-select/emote-select.component.ts
@@ -15,19 +15,34 @@ export class EmoteSelectComponent implements AfterViewInit {
     @Output()
     emoteSelectedEvent = new EventEmitter<emote>();
 
+    emoteSearchInputElement!: HTMLInputElement;
+
     // emotes
-    availableEmotes!: emote[];
+    availableEmotes = new Array<emote>();
+    filteredEmotes = new Array<emote>();
 
     constructor(private userService: UserService) { }
 
     ngAfterViewInit(): void {
         // fetch emotes
-        this.userService.getAvailableEmotes(this.userService.currentUser.userId).subscribe(emotes => this.availableEmotes = emotes);
+        this.userService.getAvailableEmotes(this.userService.currentUser.userId).subscribe(emotes => {
+            this.availableEmotes = emotes;
+            this.filteredEmotes = this.availableEmotes;
+        });
+
+        // get input element to add filter listener
+        this.emoteSearchInputElement = (document.getElementById("emoteSearchInput") as HTMLInputElement);
+        this.emoteSearchInputElement.onkeyup = () => this.filterEmotes();
+
     }
 
     emoteSelect(emote: emote) {
-        console.log(emote);
         this.emoteSelectedEvent.emit(emote);
+    }
+
+    filterEmotes() {
+        const input = this.emoteSearchInputElement.value;
+        this.filteredEmotes = this.availableEmotes.filter(emote => emote.name.includes(input));
     }
 
 }
