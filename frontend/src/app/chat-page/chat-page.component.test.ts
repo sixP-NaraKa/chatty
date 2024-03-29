fce07e1478300bff64035adb1dd5366d5cc59a7f
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2024-03-29 18:16:56 +0100

    tests: finish chat-page.component tests.

--- frontend/src/app/chat-page/chat-page.component.test.ts
+++ frontend/src/app/chat-page/chat-page.component.test.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { MockComponent } from 'ng-mocks';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, of } from 'rxjs';
 import { ChatRoomWithParticipantsExceptSelf, Settings } from '../../../../shared/types/db-dtos';
 import { ChatTabsComponent } from '../chat-tabs/chat-tabs.component';
+import { ChatComponent } from '../chat/chat.component';
+import { GroupChatUsersComponent } from '../group-chat-users/group-chat-users.component';
 import { HeaderComponent } from '../header/header.component';
 import { NotificationSummaryComponent } from '../notification-summary/notification-summary.component';
 import { UserSettingsService } from '../services/user-settings.service';
@@ -16,19 +19,25 @@ import { ChatPageComponent } from './chat-page.component';
 describe('ChatTabsComponent', () => {
     let component: ChatPageComponent;
     let fixture: ComponentFixture<ChatPageComponent>;
-    let userServiceMock: Partial<UserService> = {};
-    let websocketServiceMock: Partial<WebsocketService> = {
-        connect: jest.fn(),
-    };
+    let userServiceMock: Partial<UserService>;
+    let websocketServiceMock: Partial<WebsocketService>;
     let userSettingsServiceMock: Partial<UserSettingsService> = {
         loadUserSettings: jest.fn(),
         currentUserSettingsSubject$: new ReplaySubject<Settings>(1),
     };
 
     beforeEach(async () => {
+        userServiceMock = {};
+        websocketServiceMock = {
+            connect: jest.fn(),
+        };
+        userSettingsServiceMock.clearUserSettings = jest.fn();
+
         await TestBed.configureTestingModule({
             declarations: [
                 ChatPageComponent,
+                MockComponent(ChatComponent),
+                MockComponent(GroupChatUsersComponent),
                 MockComponent(HeaderComponent),
                 MockComponent(SliderComponent),
                 MockComponent(NotificationSummaryComponent),
@@ -42,8 +51,6 @@ describe('ChatTabsComponent', () => {
                 { provide: UserSettingsService, useValue: userSettingsServiceMock },
             ],
         }).compileComponents();
-
-        userSettingsServiceMock.clearUserSettings = jest.fn();
 
         fixture = TestBed.createComponent(ChatPageComponent);
         component = fixture.componentInstance;
@@ -78,20 +85,307 @@ describe('ChatTabsComponent', () => {
         expect(document.eventListeners!('click').length).toBe(0);
     });
 
-    test('should display the chat', () => {
-        const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
-            chatroom_id: 1,
-        };
-        websocketServiceMock.joinChatroom = jest.fn();
-        component.groupChatParticipants.length = 1;
-        component.hideDropdown = false;
+    describe('display chat', () => {
+        test('should display', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+            };
+            websocketServiceMock.joinChatroom = jest.fn();
+            component.groupChatParticipants.length = 1;
+            component.hideDropdown = false;
 
-        component.displayChat(chat as ChatRoomWithParticipantsExceptSelf);
+            component.displayChat(chat as ChatRoomWithParticipantsExceptSelf);
 
-        expect(websocketServiceMock.joinChatroom).toHaveBeenCalledTimes(1);
-        expect(websocketServiceMock.joinChatroom).toHaveBeenCalledWith(chat.chatroom_id);
-        expect(component.chatroom).toBe(chat as ChatRoomWithParticipantsExceptSelf);
-        expect(component.groupChatParticipants.length).toBe(0);
-        expect(component.hideDropdown).toBeTruthy();
+            expect(websocketServiceMock.joinChatroom).toHaveBeenCalledTimes(1);
+            expect(websocketServiceMock.joinChatroom).toHaveBeenCalledWith(chat.chatroom_id);
+            expect(component.chatroom).toBe(chat as ChatRoomWithParticipantsExceptSelf);
+            expect(component.groupChatParticipants.length).toBe(0);
+            expect(component.hideDropdown).toBeTruthy();
+        });
+
+        test('should fire loadChat', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+            };
+            const spy = jest.spyOn(component, 'displayChat').mockImplementation(() => {});
+
+            fixture.debugElement.query(By.css('app-chat-tabs')).triggerEventHandler('loadChat', chat);
+
+            expect(spy).toHaveBeenCalledTimes(1);
+            expect(spy).toHaveBeenCalledWith(chat as ChatRoomWithParticipantsExceptSelf);
+        });
+    });
+
+    describe('group chat', () => {
+        test('should show users in group chat', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+
+            component.showUsersForGroupChat();
+
+            expect(component.groupChatParticipants.length).not.toBe(0);
+            expect(component.hideDropdown).toBeFalsy();
+        });
+
+        test('should hide users in group chat', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+
+            // call it twice, because then it is hidden again
+            component.showUsersForGroupChat();
+            expect(component.groupChatParticipants.length).not.toBe(0);
+            expect(component.hideDropdown).toBeFalsy();
+
+            component.showUsersForGroupChat();
+            expect(component.groupChatParticipants.length).toBe(0);
+            expect(component.hideDropdown).toBeTruthy();
+        });
+
+        test('should call show/hide users button when fired on click', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+            // detect changes so the *ngIf directives are correctly applied
+            fixture.detectChanges();
+
+            const spy = jest.spyOn(component, 'showUsersForGroupChat').mockImplementation(() => {});
+
+            fixture.debugElement.query(By.css('#chatHeaderNavBarButtons div button')).triggerEventHandler('click', {});
+
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+
+        test('should remove user from group chat', () => {
+            const userToRemove = {
+                user_id: 2,
+                display_name: 'Test User 2',
+                creation_date: new Date(),
+            };
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                        { users: userToRemove },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+
+            websocketServiceMock.removeUserFromChatroom = jest.fn();
+            const usRemoveReturn = of(1);
+            userServiceMock.removeUserFromGroupChat = jest.fn().mockImplementation(() => usRemoveReturn);
+
+            expect(component.chatroom.chatrooms.participants).toContainEqual({ users: userToRemove });
+
+            component.onRemoveParticipantFromGroupChat(userToRemove);
+
+            expect(websocketServiceMock.removeUserFromChatroom).toHaveBeenCalledWith(
+                userToRemove.user_id,
+                chat.chatroom_id
+            );
+            expect(userServiceMock.removeUserFromGroupChat).toHaveBeenCalledWith(
+                userToRemove.user_id,
+                chat.chatroom_id
+            );
+            expect(userServiceMock.removeUserFromGroupChat).toHaveReturnedWith(usRemoveReturn);
+
+            expect(component.chatroom.chatrooms.participants).not.toContainEqual({ users: userToRemove });
+        });
+
+        test('should remove user when fired via event', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+            // detect changes so the *ngIf directives are correctly applied
+            fixture.detectChanges();
+
+            const spy = jest.spyOn(component, 'onRemoveParticipantFromGroupChat').mockImplementation(() => {});
+
+            fixture.debugElement
+                .query(By.css('app-group-chat-users'))
+                .triggerEventHandler('removeUserFromGroupChat', {});
+
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+
+        test('should add user to group chat', () => {
+            const userToAdd = {
+                user_id: 2,
+                display_name: 'Test User 2',
+                creation_date: new Date(),
+            };
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+
+            userServiceMock.addUsersToGroupChat = jest.fn().mockImplementation((_) => of(_));
+            websocketServiceMock.addUserToChatroom = jest.fn();
+
+            component.onAddParticipantToGroupChat(userToAdd);
+
+            expect(userServiceMock.addUsersToGroupChat).toHaveBeenCalledWith(userToAdd.user_id, chat.chatroom_id);
+            expect(websocketServiceMock.addUserToChatroom).toHaveBeenCalledWith(chat, userToAdd.user_id);
+
+            expect(component.chatroom.chatrooms.participants).toContainEqual({ users: userToAdd });
+        });
+
+        test('should add user when fired via event', () => {
+            const chat: Partial<ChatRoomWithParticipantsExceptSelf> = {
+                chatroom_id: 1,
+                chatrooms: {
+                    chatroom_id: 1,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [
+                        {
+                            users: {
+                                user_id: 1,
+                                display_name: 'Test User',
+                                creation_date: new Date(),
+                            },
+                        },
+                    ],
+                },
+            };
+            component.chatroom = chat as ChatRoomWithParticipantsExceptSelf;
+            // detect changes so the *ngIf directives are correctly applied
+            fixture.detectChanges();
+
+            const spy = jest.spyOn(component, 'onAddParticipantToGroupChat').mockImplementation(() => {});
+
+            fixture.debugElement
+                .query(By.css('app-group-chat-users'))
+                .triggerEventHandler('addUserToGroupChatEvent', {});
+
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('notification counter', () => {
+        test('should change notification counter', () => {
+            expect(component.notificationCounter).toBe(0);
+            component.onNotificationCounterChange(10);
+            expect(component.notificationCounter).toBe(10);
+
+            fixture.detectChanges();
+            expect(
+                fixture.debugElement.query(By.css('app-slider .sliderContentInButtonIcon')).nativeElement.innerHTML
+            ).toBe(' 10');
+            expect(
+                fixture.debugElement.query(By.css('app-slider .sliderContentOutButtonIcon')).nativeElement.innerHTML
+            ).toBe(' 10');
+        });
+
+        test('should change notification counter when fired via event', () => {
+            expect(component.notificationCounter).toBe(0);
+            const spy = jest.spyOn(component, 'onNotificationCounterChange').mockImplementation();
+
+            fixture.debugElement
+                .query(By.css('app-notification-summary'))
+                .triggerEventHandler('notificationCounterEvent', 10);
+
+            expect(spy).toHaveBeenCalledWith(10);
+        });
     });
 });
