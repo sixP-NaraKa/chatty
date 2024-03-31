c784037969b89fa6d647fb59670b604738858d71
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2024-03-31 03:17:46 +0200

    tests: finish chat-tabs.component tests. Reformat db-dtos file.

--- frontend/src/app/chat-tabs/chat-tabs.component.test.ts
+++ frontend/src/app/chat-tabs/chat-tabs.component.test.ts
@@ -1,9 +1,14 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent, MockService } from 'ng-mocks';
 import { of } from 'rxjs';
-import { ChatRoomWithParticipantsExceptSelf } from '../../../../shared/types/db-dtos';
+import {
+    ChatMessageWithUser,
+    ChatRoomWithParticipantsExceptSelf,
+    MessageReaction,
+} from '../../../../shared/types/db-dtos';
 import { ApplicationUser } from '../auth/auth.service';
 import { GroupChatWindowComponent } from '../group-chat-window/group-chat-window.component';
 import { NotificationService } from '../services/notification.service';
@@ -16,16 +21,10 @@ describe('ChatTabsComponent', () => {
     let component: ChatTabsComponent;
     let fixture: ComponentFixture<ChatTabsComponent>;
     let userServiceMock = MockService(UserService);
-    let notificationServiceSpy = MockService(NotificationService);
+    let notificationServiceMock = MockService(NotificationService);
 
     // via "normal" mocking
-    let websocketServiceMock: Partial<WebsocketService> = {
-        getNewChatroom: jest.fn().mockReturnValue(of()),
-        getChatMessage: jest.fn().mockReturnValue(of()),
-        listenForRemoveChatroom: jest.fn().mockReturnValue(of()),
-        getNewEmoteReaction: jest.fn().mockReturnValue(of()),
-        getChangedAvailabilities: jest.fn().mockReturnValue(of()),
-    };
+    let websocketServiceMock: Partial<WebsocketService>;
 
     const fakeUser: ApplicationUser = {
         access_token: 'access token',
@@ -34,6 +33,14 @@ describe('ChatTabsComponent', () => {
     };
 
     beforeEach(async () => {
+        websocketServiceMock = {
+            getNewChatroom: jest.fn().mockReturnValue(of()),
+            getChatMessage: jest.fn().mockReturnValue(of()),
+            listenForRemoveChatroom: jest.fn().mockReturnValue(of()),
+            getNewEmoteReaction: jest.fn().mockReturnValue(of()),
+            getChangedAvailabilities: jest.fn().mockReturnValue(of()),
+        };
+
         await TestBed.configureTestingModule({
             declarations: [
                 ChatTabsComponent,
@@ -44,7 +51,7 @@ describe('ChatTabsComponent', () => {
             providers: [
                 { provide: UserService, useValue: userServiceMock },
                 { provide: WebsocketService, useValue: websocketServiceMock },
-                { provide: NotificationService, useValue: notificationServiceSpy },
+                { provide: NotificationService, useValue: notificationServiceMock },
             ],
         }).compileComponents();
 
@@ -55,11 +62,13 @@ describe('ChatTabsComponent', () => {
 
         fixture = TestBed.createComponent(ChatTabsComponent);
         component = fixture.componentInstance;
+        component.audioElementUnreadMessage.play = jest.fn();
         fixture.detectChanges();
     });
 
     afterEach(async () => {
         jest.clearAllMocks();
+        // jest.resetAllMocks();
     });
 
     test('should create', () => {
@@ -187,7 +196,6 @@ describe('ChatTabsComponent', () => {
                 p_id: 2,
                 user_id: 1,
             };
-            userServiceMock.getChatroomsForUserWithParticipantsExceptSelf = jest.fn().mockReturnValue(of());
             websocketServiceMock.getNewChatroom = jest.fn().mockReturnValue(of(chatroom));
             websocketServiceMock.joinChatroom = jest.fn();
             userServiceMock.getSingleChatroomForUserWithParticipantsExceptSelf = jest
@@ -205,5 +213,156 @@ describe('ChatTabsComponent', () => {
             expect(component.chatrooms.length).toBe(1);
             expect(component.chatrooms).toContain(chatroom);
         });
+
+        test('should listen for new messages from not open chatrooms', () => {
+            const chatroom: ChatRoomWithParticipantsExceptSelf = {
+                chatroom_id: 2,
+                chatrooms: {
+                    chatroom_id: 2,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [],
+                },
+                p_id: 2,
+                user_id: 1,
+            };
+            const message: Partial<ChatMessageWithUser> = {
+                chatroom_id: 2,
+                users: {
+                    user_id: 1,
+                    creation_date: new Date(),
+                    display_name: 'Test User Display Name',
+                },
+            };
+            const ofMessage = of(message);
+
+            websocketServiceMock.getChatMessage = jest.fn().mockReturnValue(ofMessage);
+            userServiceMock.getSingleChatroomForUserWithParticipantsExceptSelf = jest
+                .fn()
+                .mockReturnValue(of(chatroom));
+            notificationServiceMock.newUnread = jest.fn();
+
+            component.ngAfterContentInit();
+
+            expect(websocketServiceMock.getChatMessage).toHaveReturnedWith(ofMessage);
+            expect(userServiceMock.getSingleChatroomForUserWithParticipantsExceptSelf).toHaveBeenCalledWith(
+                message.chatroom_id
+            );
+            expect(userServiceMock.getSingleChatroomForUserWithParticipantsExceptSelf).toHaveBeenCalledTimes(1);
+
+            expect(component.chatrooms.length).toBe(1);
+            expect(component.chatrooms).toContain(chatroom);
+            expect(notificationServiceMock.newUnread).toHaveBeenCalledTimes(1);
+        });
+
+        test('should listen for chat removals', () => {
+            const chatroom: ChatRoomWithParticipantsExceptSelf = {
+                chatroom_id: 2,
+                chatrooms: {
+                    chatroom_id: 2,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [],
+                },
+                p_id: 2,
+                user_id: 1,
+            };
+            component.chatrooms.push(chatroom);
+
+            websocketServiceMock.listenForRemoveChatroom = jest.fn().mockReturnValue(of(2));
+
+            component.ngAfterContentInit();
+
+            expect(component.chatrooms.length).toBe(0);
+        });
+
+        test('should listen for new message reactions from not open chats', () => {
+            const reaction: Partial<MessageReaction> = {
+                emote: {
+                    emote: 'Emote',
+                    emote_id: 1,
+                    name: 'Emote Name',
+                },
+            };
+            const chatroom: ChatRoomWithParticipantsExceptSelf = {
+                chatroom_id: 2,
+                chatrooms: {
+                    chatroom_id: 2,
+                    created_at: new Date(),
+                    created_by: 1,
+                    isgroup: true,
+                    name: 'Test Group',
+                    participants: [],
+                },
+                p_id: 2,
+                user_id: 1,
+            };
+            component.chatrooms.push(chatroom);
+            websocketServiceMock.getNewEmoteReaction = jest.fn().mockReturnValue(of([2, 1, 1, reaction]));
+            notificationServiceMock.newUnread = jest.fn();
+
+            websocketServiceMock.listenForRemoveChatroom = jest.fn().mockReturnValue(of(2));
+
+            component.ngAfterContentInit();
+
+            expect(notificationServiceMock.newUnread).toHaveBeenCalledTimes(1);
+        });
+
+        test('should listen availability status changes', () => {
+            websocketServiceMock.getChangedAvailabilities = jest.fn().mockReturnValue(of([1, 2, 3]));
+
+            component.ngAfterContentInit();
+
+            expect(websocketServiceMock.getChangedAvailabilities).toHaveBeenCalledTimes(1);
+            expect(component.availabilityStatusUsers.length).toBe(3);
+            expect(component.availabilityStatusUsers).toEqual([1, 2, 3]);
+        });
+    });
+
+    test('should show group chat creation window + should fire on click', () => {
+        component.onCreateGroupChatButtonClick();
+        expect(component.showGroupChatCreateWindow).toBeTruthy();
+
+        component.showGroupChatCreateWindow = false;
+        const spy = jest.spyOn(component, 'onCreateGroupChatButtonClick');
+
+        fixture.debugElement.query(By.css('.user-search-group-chat button')).triggerEventHandler('click', {});
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(component.showGroupChatCreateWindow).toBeTruthy();
+    });
+
+    test('should hide group chat creation window + should fire on click', () => {
+        component.onCreateGroupChatClosed();
+        expect(component.showGroupChatCreateWindow).toBeFalsy();
+
+        component.showGroupChatCreateWindow = true;
+        const spy = jest.spyOn(component, 'onCreateGroupChatClosed');
+
+        fixture.debugElement.query(By.css('app-group-chat-window')).triggerEventHandler('groupChatClosedEvent', {});
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(component.showGroupChatCreateWindow).toBeFalsy();
+    });
+
+    test('should create group chat + should fire on click', () => {
+        const chatroom: Partial<ChatRoomWithParticipantsExceptSelf> = {
+            chatroom_id: 100,
+        };
+        component.onCreateGroupChatEvent(chatroom as ChatRoomWithParticipantsExceptSelf);
+        expect(component.chatrooms.length).toBe(1);
+        expect(component.chatrooms).toContain(chatroom as ChatRoomWithParticipantsExceptSelf);
+
+        component.chatrooms.length = 0;
+        const spy = jest.spyOn(component, 'onCreateGroupChatEvent');
+
+        fixture.debugElement
+            .query(By.css('app-group-chat-window'))
+            .triggerEventHandler('groupChatCreatedEvent', chatroom);
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(component.chatrooms.length).toBe(1);
+        expect(component.chatrooms).toContain(chatroom as ChatRoomWithParticipantsExceptSelf);
     });
 });
--- shared/types/db-dtos.ts
+++ shared/types/db-dtos.ts
@@ -1,73 +1,73 @@
 type User = {
-    user_id: number,
-    display_name: string,
-    creation_date: Date
-}
+    user_id: number;
+    display_name: string;
+    creation_date: Date;
+};
 
 type UserIdDisplayName = {
     users: {
-        user_id: number,
-        display_name: string
-    }
-}
+        user_id: number;
+        display_name: string;
+    };
+};
 
 type Chatroom = {
-    chatroom_id: number,
-    name: string | null,
-    isgroup: boolean,
-    created_by: number | null,
-    created_at: Date | null
+    chatroom_id: number;
+    name: string | null;
+    isgroup: boolean;
+    created_by: number | null;
+    created_at: Date | null;
 };
 
 type ChatRoomWithParticipantsExceptSelf = {
-    p_id: number,
-    user_id: number,
-    chatroom_id: number,
+    p_id: number;
+    user_id: number;
+    chatroom_id: number;
     chatrooms: Chatroom & {
         participants: {
-            users: User
+            users: User;
         }[];
     };
-}
+};
 
 type Reaction = {
-    reactions_id: number,
-    msg_id: number,
-    emote_id: number,
-    user_id: number | null
+    reactions_id: number;
+    msg_id: number;
+    emote_id: number;
+    user_id: number | null;
 };
 
 type Emote = {
-    emote_id: number,
-    emote: string,
-    name: string
+    emote_id: number;
+    emote: string;
+    name: string;
 };
 
-type MessageReaction = (Reaction & {
-    emote: Emote,
-    users: User
-});
+type MessageReaction = Reaction & {
+    emote: Emote;
+    users: User;
+};
 
 type ChatMessage = {
-    msg_id: number,
-    posted_at: Date,
-    msg_content: string,
-    user_id: number,
-    chatroom_id: number,
-    isimage: boolean,
-    isfile: boolean,
-    file_uuid: string
+    msg_id: number;
+    posted_at: Date;
+    msg_content: string;
+    user_id: number;
+    chatroom_id: number;
+    isimage: boolean;
+    isfile: boolean;
+    file_uuid: string;
 };
 
 type ChatMessageWithUser = ChatMessage & {
-    users: User,
-    reactions: MessageReaction[],
+    users: User;
+    reactions: MessageReaction[];
 };
 
 type Participant = {
-    p_id: number,
-    user_id: number,
-    chatroom_id: number
+    p_id: number;
+    user_id: number;
+    chatroom_id: number;
 };
 
 type ChatroomWithMessages = Chatroom & {
@@ -76,41 +76,41 @@ type ChatroomWithMessages = Chatroom & {
 };
 
 type NotificationUnread = {
-    notification_id: number,
-    user_id: number,
-    chatroom_id: number,
-    type: string,
-    content: string,
-    originated_from: number,
-    date: Date
+    notification_id: number;
+    user_id: number;
+    chatroom_id: number;
+    type: string;
+    content: string;
+    originated_from: number;
+    date: Date;
 };
 
 type Notification = NotificationUnread & {
-    users: User,
-    originated_from_user: User,
-    chatrooms: Chatroom
+    users: User;
+    originated_from_user: User;
+    chatrooms: Chatroom;
 };
 
 type Settings = {
-    settings_id: number
-    user_id: number
-    filter: string
-    font_size: string
-    embed_yt_videos: boolean
+    settings_id: number;
+    user_id: number;
+    filter: string;
+    font_size: string;
+    embed_yt_videos: boolean;
 };
 
 export {
-    User,
-    Chatroom,
-    Participant,
-    ChatRoomWithParticipantsExceptSelf,
-    ChatroomWithMessages,
     ChatMessageWithUser,
-    UserIdDisplayName,
-    MessageReaction,
-    NotificationUnread,
-    Notification,
-    Settings,
+    ChatRoomWithParticipantsExceptSelf,
+    Chatroom,
+    ChatroomWithMessages,
     Emote,
+    MessageReaction,
+    Notification,
+    NotificationUnread,
+    Participant,
     Reaction,
+    Settings,
+    User,
+    UserIdDisplayName,
 };
